e55573681757864ccb27c1469bbfaf44f822f7b0
Badri Shiva <82711831+shivaBadri@users.noreply.github.com>
2026-07-18 12:04:11 +0530

    Create seed.ts

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import bcrypt from "bcrypt";
+import { PrismaClient, Role } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const passwordHash = await bcrypt.hash("Admin@123", 10);
+
+  await prisma.user.upsert({
+    where: {
+      email: "admin@sharanyapropertieshub.com",
+    },
+    update: {},
+    create: {
+      name: "Administrator",
+      email: "admin@sharanyapropertieshub.com",
+      passwordHash,
+      role: Role.ADMIN,
+      permissions: [],
+    },
+  });
+
+  console.log("✅ Admin user created");
+}
+
+main()
+  .catch(console.error)
+  .finally(() => prisma.$disconnect());
